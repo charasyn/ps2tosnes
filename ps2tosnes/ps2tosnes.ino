e5cd3a0f2939e09ed32e5dcbe71dbc96144c8153
Cooper Harasyn <coop.rocks.123e@gmail.com>
2015-06-13 09:55:06 -0400

    Initial commit

---
+++ ps2tosnes/ps2tosnes.ino
@@ -0,0 +1,107 @@
+#include <ps2.h>
+
+#define P_DATA  2
+#define P_CLOCK 3
+
+// pins 8-12
+#define S_DATA  8
+#define S_LATCH 9
+#define S_CLOCK 10
+
+#define checkLatch (PINB&(1<<(S_LATCH-8)))
+#define checkClock (PINB&(1<<(S_CLOCK-8)))
+inline void writeData(byte x){ if(x) PORTB|=1; else PORTB&=0xfe; }
+
+inline byte reverse(byte b) { // thanks "sth" from StackOverflow
+   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+   return b;
+}
+
+byte mdata[3];
+byte output[2];
+byte bitout[32];
+byte i;
+byte led;
+signed char x,y;
+
+PS2 mouse(P_CLOCK, P_DATA);
+void mouse_init()
+{
+  mouse.write(0xff);  // reset
+  mouse.read();
+  mouse.read();
+  mouse.read();
+  /* // wheel mouse mode
+  
+  mouse.write(0xf3);  // sample rate
+  mouse.read();  // ack
+  mouse.write(0xc8);  // sample rate
+  mouse.read();  // ack
+  
+  mouse.write(0xf3);  // sample rate
+  mouse.read();  // ack
+  mouse.write(0x64);  // sample rate
+  mouse.read();  // ack
+  
+  mouse.write(0xf3);  // sample rate
+  mouse.read();  // ack
+  mouse.write(0x50);  // sample rate
+  mouse.read();  // ack
+  
+  mouse.write(0xf2);  // get mouse id
+  mouse.read();  // ack
+  mouse.read();  // mouse id (we would care about this if we actually scrolled)*/
+  
+  delayMicroseconds(100);
+}
+void mouse_update()
+{
+  mouse.write(0xeb);
+  mouse.read();
+  mdata[0] = mouse.read();
+  mdata[1] = mouse.read();
+  mdata[2] = mouse.read();
+}
+
+void setup() {
+  mouse_init();
+  pinMode(S_DATA,OUTPUT);
+  pinMode(S_LATCH,INPUT_PULLUP);
+  pinMode(S_CLOCK,INPUT_PULLUP);
+  digitalWrite(S_DATA,LOW);
+  led=0;
+}
+
+void loop() {
+  mouse_update();
+  
+  x=(signed char)mdata[1];
+  y=(signed char)mdata[2];
+  output[0]=reverse(~((min(abs(y),0x7f))|((y<0)?0x00:0x80))); //y
+  output[1]=reverse(~((min(abs(x),0x7f))|((x<0)?0x80:0x00))); //x
+  
+  for(i=0;i<16;i++){
+    bitout[i]=1;
+  }
+  for(;i<32;i++){
+    bitout[i]=output[(i>>3)&1]&(1<<(i&7));
+  }
+  bitout[8]=mdata[0]&0x02?0:1;
+  bitout[9]=mdata[0]&0x01?0:1;
+  bitout[15]=0;
+  i=0;
+  if(led){led=0;PORTB&=~(1<<(13-8));}else{led=1;PORTB|=1<<(13-8);}
+  
+  noInterrupts();
+  while(!checkLatch);
+  writeData(1);
+  while(checkLatch);
+  for(;i<32;i++){
+    writeData(bitout[i]);
+    while(checkClock);  //wait for falling
+    while(!checkClock); //wait for rising
+  }
+  interrupts();
+}
